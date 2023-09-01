f156fca8662a5dec262df378ca69c3ee1d59b0e9
Shreya Mandlik <138625027+shreyamandlik@users.noreply.github.com>
2023-09-02 03:35:44 +0530

    Add files via upload

---
+++ requirement.js
@@ -0,0 +1,55 @@
+//Requirement
+
+//Book Management company
+//Books
+//ISBN, title, Pubdate,lang, numpage, author[, category
+
+//Authors
+//id, author name, books[]
+
+//Publication
+//id, name, books[]
+
+
+//have to design code and api over this
+//GET REQUEST
+//1.Books
+//Need API for
+//get all books -Done
+//get book based on isbn-Done
+//get book based on category-Done
+//get book based on languages-Done
+
+
+//2.Authors
+//Need API for
+//To get a specific author
+//to get all authors
+//to get list of authors based on books
+
+
+//3.Publications
+//Need API for
+//to get all publication
+//to get specific publications
+//to get list of publications based pn books
+
+/*--------------------------------------------------------*/
+
+//POST REQUEST
+//1.Add new Books
+//2.Add new author
+//3.Add new Publication
+
+/*--------------------------------------------------------*/
+
+//PUT REQUEST
+//Update book details if author is changed--done
+//Update book details if author is changed--done
+
+/*--------------------------------------------------------*/
+
+//DELETE REQUEST
+//delete a book
+//delete a author from book
+//delete author from book and related book from author
